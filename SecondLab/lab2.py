386310e2c4008109848919a82784a18ce38b6fa4
1goshan1 <burdinskiygv@gmail.com>
2025-10-28 13:35:05 +0400

    42 братух

--- SecondLab/lab2.py
+++ SecondLab/lab2.py
@@ -32,7 +32,7 @@ def calculateBarycentric(x, y, x0, y0, x1, y1, x2, y2):
 
 
 def calcNormal(x0, y0, z0, x1, y1, z1, x2, y2, z2):
-    return ((y1 - y2) * (z1 - z0) - (z1 - z0) * (y1 - y0), -((x1 - x2) * (z1 - z0) - (z1 - z2) * (x1 - x0)),
+    return ((y1 - y2) * (z1 - z0) - (z1 - z2) * (y1 - y0), -((x1 - x2) * (z1 - z0) - (z1 - z2) * (x1 - x0)),
             (x1 - x2) * (y1 - y0) - (y1 - y2) * (x1 - x0))
 
 
